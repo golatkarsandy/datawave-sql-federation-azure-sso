3f2a5c183a5a8bf39c0bb80d906827c100c7a3e2
golatkarsandy <golatkarsandy@gmail.com>
2026-04-25 15:23:38 -0500

    Initial commit - SSO-enabled Trino project

---
+++ docker/init-db/mysql.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE tracking(
+order_id INT,
+shipment_status VARCHAR(50)
+);
+
+INSERT INTO tracking VALUE
+(1,'Delivered'),
+(2, 'In Transit');
---
+++ docker/init-db/postgres.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_name TEXT
+);
+
+INSERT INTO orders VALUES
+(1, 'Acme Corp'),
+(2, 'Globex');
